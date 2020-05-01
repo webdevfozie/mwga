21da80dfd92b584ca4be2ad11d15b4832388d0d0
Artem Frolov <36102566+webdevfozie@users.noreply.github.com>
2020-05-01 13:28:25 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,102 @@
+$(document).ready(function () {
+	$('.main-slider__body').slick({
+			dots: true,
+			autoplay: true,
+			autoplaySpeed: 4000
+		}),
+		$('.portfolio-slider__body').slick({
+			dots: true,
+			arrow: true,
+			slidesToShow: 3,
+			slidesToScroll: 3,
+			autoplay: true,
+			autoplaySpeed: 4000,
+			responsive: [{
+					breakpoint: 960,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2,
+					}
+				},
+				{
+					breakpoint: 630,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+					}
+				}
+			]
+		});
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+
+	let tab = document.querySelectorAll('.tab'),
+		tabsBody = document.querySelector('.tabs'),
+		tabContent = document.querySelectorAll('.tab-text');
+
+	hideTabContent = (a) => {
+		for (let i = a; i < tabContent.length; i++) {
+			tabContent[i].classList.remove('show');
+			tabContent[i].classList.add('hide');
+			tab[i].classList.remove('tab-active');
+		}
+	}
+
+	hideTabContent(1);
+
+	showTabContent = (b) => {
+		if (tabContent[b].classList.contains('hide')) {
+			tabContent[b].classList.remove('hide');
+			tabContent[b].classList.add('show');
+			tab[b].classList.add('tab-active');
+		}
+	}
+
+	tabsBody.addEventListener('click', (e) => {
+		let target = e.target;
+		if (target && target.classList.contains('tab')) {
+			for (let i = 0; i < tab.length; i++) {
+				if (target == tab[i]) {
+					hideTabContent(0);
+					showTabContent(i);
+					break;
+				}
+			}
+		}
+	})
+
+	//Плавный скролл
+	var scroll = new SmoothScroll('a[href*="#"]', {
+		speed: 700
+	});
+
+	//Модальное окно
+	const modalBody = document.querySelector('.modal_body'),
+		closeBtn = modalBody.querySelector('.close-button'),
+		modalBtn1 = document.querySelector('#modal-btn-1'),
+		modalBtn2 = document.querySelector('#modal-btn-2'),
+		html = document.querySelector('html');
+
+	modalBtn1.addEventListener('click', () => {
+		modalBody.classList.remove('hide');
+		html.classList.add('nscroll');
+	})
+
+	modalBtn2.addEventListener('click', () => {
+		modalBody.classList.remove('hide');
+		html.classList.add('nscroll');
+	})
+
+	closeBtn.addEventListener('click', () => {
+		modalBody.classList.add('hide');
+		html.classList.remove('nscroll');
+	})
+	document.addEventListener('click', (e) => {
+		let target = e.target;
+		if (target && target.classList.contains('modal_body')) {
+			modalBody.classList.add('hide');
+			html.classList.remove('nscroll');
+		}
+	})
+});
